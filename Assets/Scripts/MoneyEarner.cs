468a65889ba9adc0bcbf91df9a388cc1c59b2146
dan52rusik <baryshevdanila@gmail.com>
2023-01-08 15:43:51 +0300

    I am add Particles Noti Pri Nazatii

--- Assets/Scripts/MoneyEarner.cs
+++ Assets/Scripts/MoneyEarner.cs
@@ -8,15 +8,21 @@ public class MoneyEarner : MonoBehaviour
     private int _coins;
     [SerializeField] private int _coinsMultiplier;
     [SerializeField] private TMP_Text _coinsText;
+    [SerializeField] private GameObject _clickFX;
+    [SerializeField] private RectTransform _buttonPosition;
     // Start is called before the first frame update
     void Start()
     {
         if (PlayerPrefs.HasKey("coins"))
-        _coins = PlayerPrefs.GetInt("coins");
+        {
+            _coins = PlayerPrefs.GetInt("coins");
+            _coinsText.text = _coins.ToString();
+        }
     }
 
     public void EarnMoney()
     {
+        Instantiate(_clickFX, _buttonPosition.position.normalized, Quaternion.identity);
         _coins += _coinsMultiplier;
         PlayerPrefs.SetInt("coins", _coins);
         _coinsText.text = _coins.ToString();
