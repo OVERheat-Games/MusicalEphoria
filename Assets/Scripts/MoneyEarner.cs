59204d3b012e5a1700c6a947b4b2c0f97c89e903
dan52rusik <baryshevdanila@gmail.com>
2023-01-08 14:47:06 +0300

    I am add Script MoneyEarner

---
+++ Assets/Scripts/MoneyEarner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyEarner : MonoBehaviour
+{
+    private int _coins;
+    [SerializeField] private int _coinsMultiplier;
+    [SerializeField] private TMP_Text _coinsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("coins"))
+        _coins = PlayerPrefs.GetInt("coins");
+    }
+
+    public void EarnMoney()
+    {
+        _coins += _coinsMultiplier;
+        PlayerPrefs.SetInt("coins", _coins);
+        _coinsText.text = _coins.ToString();
+    }
+}
